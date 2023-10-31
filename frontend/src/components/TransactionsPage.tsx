b8255de4fb67aa57b19229052f1f8bc316082134
Ethan Walker <ethan.walker830@gmail.com>
2023-10-30 21:02:42 -0700

    adding env var and first attempt at making a request

--- frontend/src/components/TransactionsPage.tsx
+++ frontend/src/components/TransactionsPage.tsx
@@ -3,9 +3,14 @@ import { Box } from "@mui/material";
 import DropDownMenu from "./DropDownMenu";
 import DataTable from "./DataTable";
 
-var SELECTED_VALUES = {year: 0, month: 0}
+const base_url = process.env.REACT_APP_HOST_URI
 
-function getTransactions(year: number, month: number) {
+async function getTransactions(year: number, month: number) {
+  // const response = await fetch(base_url + `/transactions/${year}/${month}`);
+  // const data = await response.json();
+  // console.log(data);
+  // console.log(base_url + `/transactions/${year}/${month}`)
+  // console.log(process.env.NODE_ENV)
   return;
 }
 
@@ -31,6 +36,7 @@ export default function TransactionsPage() {
     </Box>
     <Box>Current year: {selection.year}</Box>
     <Box>Current month: {selection.month}</Box>
+    {/* <DataTable /> */}
     </div>
   );
 }
